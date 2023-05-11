4610258085329deb6dddda86536d676c9658eade
FelipeMirandaM <felipemiranda341@gmail.com>
2023-05-11 10:12:07 -0400

    Configuracion por ambiente

--- src/main/java/com/curso/springboot/learnspringboot/CourseController.java
+++ src/main/java/com/curso/springboot/learnspringboot/CourseController.java
@@ -10,10 +10,12 @@ import java.util.List;
 public class CourseController {
 
     @RequestMapping("/courses")
-    public List<Course> retrieveAllCourses(){
+    public List<Course> retrieveAllCourses() {
         return Arrays.asList(
-                new Course(1,"Learn AWS", "in28minutes"),
-                new Course(2,"Learn DevOps","in28minutes")
+                new Course(1, "Learn AWS", "in28minutes"),
+                new Course(2, "Learn DevOps", "in28minutes"),
+                new Course(3, "Learn Azure", "in28minutes"),
+                new Course(4, "Learn GCP", "in28minutes")
         );
     }
 
