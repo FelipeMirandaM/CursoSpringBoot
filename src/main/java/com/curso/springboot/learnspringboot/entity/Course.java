d2e1098e2135b3de65df0476f111391241de2727
FelipeMirandaM <felipemiranda341@gmail.com>
2023-05-15 18:43:09 -0400

    Crud jdbc con h2

--- src/main/java/com/curso/springboot/learnspringboot/entity/Course.java
+++ src/main/java/com/curso/springboot/learnspringboot/entity/Course.java
@@ -10,6 +10,11 @@ public class Course {
         this.name = name;
         this.author = author;
     }
+    public Course() {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+    }
 
     public long getId() {
         return id;
@@ -21,6 +26,18 @@ public class Course {
 
     public String getAuthor() {
         return author;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
     }
 
     @Override
